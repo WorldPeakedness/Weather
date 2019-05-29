b34dbd86b10f57e0cbb4b5ed1c87bbf8edc32bfd
shijie <shijie@email.com>
2019-05-29 22:56:31 +0800

    github桌面版测试

--- CoolWeather/app/src/main/java/android/coolweeather/com/coolweather/WeatherActivity.java
+++ CoolWeather/app/src/main/java/android/coolweeather/com/coolweather/WeatherActivity.java
@@ -16,6 +16,7 @@ import android.support.v4.widget.SwipeRefreshLayout;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.Button;
@@ -78,6 +79,8 @@ public class WeatherActivity extends AppCompatActivity {
         }
         ActionBar actionBar=getSupportActionBar();
         actionBar.hide();
+
+        Log.d("GitHubdemo", "onCreate: ");
 
         initView();
 
